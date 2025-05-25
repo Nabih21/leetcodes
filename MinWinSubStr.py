c3337e0317c5b6b8cc911e4f91bb3b27ad14454b
Nabih21 <nabih21elhelou10@hotmail.com>
2025-05-24 21:14:34 -0400

    solution solved

--- MinWinSubStr.py
+++ MinWinSubStr.py
@@ -1,4 +1,3 @@
-
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
@@ -7,28 +6,42 @@ class Solution:
         minLeft, minRight = 0, 0 
         charMap = {}
         count = 1001
+        currentValues, totalValues = 0, 0 
+
 
         for char in t : 
+            if setMap.get(char, 0 )  == 0 : 
+                totalValues += 1 
             setMap[char] = setMap.get(char, 0 ) + 1
             charMap[char] = 0
 
         
         if setMap.get(s[left], 0 ) >= 1 :
             charMap[s[left]] = 1
+            if charMap[s[left]] >= setMap[s[left]] : 
+                currentValues += 1
             if len(t) == 1 : 
                 return s[0]
+
+
         
         while right < len(s) : 
             if setMap.get(s[right], 0) > 0 and not right == left : 
                 charMap[s[right]] = charMap.get(s[right], 0) +  1
+                
+                if charMap[s[right]] == setMap[s[right]] : 
+                    currentValues += 1
             
-            while  all(charMap[k] >= setMap[k] for k in setMap) :
+            while  currentValues >= totalValues :
                 if right - left + 1 < count : 
                     minLeft = left
                     minRight = right 
                     count = right - left + 1
+
                 if  setMap.get(s[left], 0) > 0  :
                     charMap[s[left]] -= 1
+                    if charMap[s[left]] < setMap[s[left]] : 
+                        currentValues -= 1
                 left += 1
                 
             right += 1
@@ -42,12 +55,12 @@ class Solution:
 
 if __name__ == "__main__":
     solution = Solution()
-    # s = "ADOBECODEBANCEEEE"
-    # t = "ABC"
+    s = "ADOBECODEBANCEEEE"
+    t = "ABC"
     # s = "aab"
     # t = "aa"
     # s = "OUZODYXAZV" 
     # t = "XYZ"
-    s="aaaaaaaaaaaabbbbbcdd"
-    t="abcdd"
+    # s="aaaaaaaaaaaabbbbbcdd"
+    # t="abcdd"
     print(solution.minWindow(s, t))  # Output the result
